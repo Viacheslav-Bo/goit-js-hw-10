9d78bf2fe53bd2c3bdbf201e1d85b374fc09a43b
Viacheslav-Bo <zotikivna@gmail.com>
2025-10-30 22:05:19 +0200

    done

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,53 +1,40 @@
-// Напиши скрипт, який після сабміту форми створює проміс.
-// В середині колбека цього промісу через вказану користувачем
-// кількість мілісекунд проміс має виконуватися (при fulfilled) або
-// відхилятися (при rejected), залежно від обраної опції в радіокнопках.
-// Значенням промісу, яке передається як аргумент у методи resolve/reject,
-//  має бути значення затримки в мілісекундах.
-
-// Створений проміс треба опрацювати у відповідних для вдалого/невдалого
-// виконання методах.
-
-// Якщо проміс виконується вдало, виводь у консоль наступний рядок,
-// де delay — це значення затримки виклику промісу в мілісекундах.
-
-// `✅ Fulfilled promise in ${delay}ms`
-
-// `❌ Rejected promise in ${delay}ms`
-
-// Описаний у документації
 import iziToast from 'izitoast';
-// Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
 
-const form = document.querySelector('.form');
-console.log(form);
+const btnSubmit = document.querySelector('button');
+const inputDelay = document.querySelector('input[type="number"]');
 
-form.addEventListener('click', evt => {
-  const btnSubmit = document.querySelector('button');
-  //   console.log(btnSubmit);
+btnSubmit.disabled = true;
+
+inputDelay.addEventListener('input', () => {
+  const states = document.querySelector('input[name="state"]:checked');
+  btnSubmit.disabled = false;
+});
+
+btnSubmit.addEventListener('click', evt => {
+  evt.preventDefault();
 
   const promise = new Promise((resolve, reject) => {
     const delay = Number(document.querySelector('input').value);
-    const isSuccess = document.querySelector('input[value="fulfilled"]');
-    console.log(isSuccess);
-    const radioReject = document.querySelector('input[value="rejected"]');
-    console.log(radioReject);
+    const state = document.querySelector('input[name="state"]:checked').value;
 
     setTimeout(() => {
-      if (isSuccess !== radioReject) {
+      if (state === 'fulfilled') {
+        iziToast.show({
+          message: `✅ Fulfilled promise in ${delay}ms`,
+          position: 'topRight',
+          backgroundColor: 'rgb(163, 255, 194)',
+        });
         resolve(`✅ Fulfilled promise in ${delay}ms`);
       } else {
+        iziToast.show({
+          message: `❌ Rejected promise in ${delay}ms`,
+          position: 'topRight',
+          backgroundColor: 'rgba(247, 164, 185, 1)',
+        });
         reject(`❌ Rejected promise in ${delay}ms`);
       }
     }, delay);
   });
-
-  promise
-    .then(value => {
-      console.log(value); // "Success! Value passed to resolve function"
-    })
-    .catch(error => {
-      console.log(error); // "Error! Error passed to reject function"
-    });
+  promise.then(console.log).catch(console.log);
 });
